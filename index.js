218444488abe4fb77c4d892dc62907832bef903b
Rafiulsam <rafiulsam69@gmail.com>
2024-05-11 03:25:06 +0600

    chefs id request

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors =require('cors')
+const cors = require('cors')
 const app = express()
 const port = 5000
 
@@ -7,14 +7,20 @@ const chefs = require('./data/chefs.json')
 
 app.use(cors())
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('server is running')
 })
 
-app.get('/chefs', (req, res)=>{
+app.get('/chefs', (req, res) => {
     res.send(chefs)
 })
 
-app.listen(port, ()=>{
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = chefs.find(c => c.id === id)
+    res.send(chef)
+})
+
+app.listen(port, () => {
     console.log('flavors of india server is running at port', port)
 })
